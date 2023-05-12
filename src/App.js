3c9d9bddf5012f4bf8d010bda48fd99fe5ca079d
egmar afonso <78624465+afonsoegmar@users.noreply.github.com>
2023-05-12 17:20:49 -0300

    lojinha com bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 
+import {ToastContainer, toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 import Lojinha from './Lojinha'
 import Menu from './Menu';
 
@@ -10,17 +13,21 @@ function App() {
     return (
         <div className="App">
 
-            <Menu />
+            <ToastContainer/>
 
-            {
-                login == false ?
-                    <>
-                        <p> Faça login para continuar </p>
-                        <button onClick={()=> setLogin(true) } > Logar </button>
-                    </>
-                :
-                    <Lojinha setLogin={ setLogin } />
-            }
+            <Menu />
+            <div className='container'>
+
+                {
+                    login == false ?
+                        <>
+                            <p> Faça login para continuar </p>
+                            <button className="btn btn-primary" onClick={()=> setLogin(true) } > Logar </button>
+                        </>
+                    :
+                        <Lojinha setLogin={ setLogin } />
+                }
+            </div>
 
         </div>
     );
--- src/Lojinha.js
+++ src/Lojinha.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 
+import { toast } from 'react-toastify'
+
 function Lojinha( props ){
 
     const [ valor, setValor ] = useState(0)
@@ -8,7 +10,7 @@ function Lojinha( props ){
     // Crie uma função pra mudar o valor e mostrar um alert()
     function finalizaCompra(){
         setValor(0)
-        alert("Compra finalizada com sucesso!")
+        toast("Compra finalizada com sucesso!")
     }
 
     function realizaCompra( preco ){
@@ -18,29 +20,71 @@ function Lojinha( props ){
 
     return(
         <div>
-            <button onClick={()=> props.setLogin(false) }> Sair </button>
+            <button className='btn btn-secondary mb-4' onClick={()=> props.setLogin(false) }> Sair </button>
 
             <h1> Lojinha </h1>
-            <p> Itens no carrinho: { itens } </p>
-            <p> Valor total: R$ { valor } </p>
-            <button onClick={ ()=> finalizaCompra() } > Finalizar compra </button>
+            <p> Itens no carrinho: <span className='bagde bg-primary '> { itens }</span> </p>
+            <p className='alert alert-info'> Valor total: R$ { valor } </p>
+            <button className='btn btn-success ' onClick={ ()=> finalizaCompra() } > Finalizar compra </button>
 
             <hr/>
 
-            <p> 
-                Camisa Polo - R$ 79,90 
-                <button onClick={ ()=> realizaCompra(79.90) } >Comprar</button>
-            </p>
+            <div className='d-flex justify-content-around'>
+                <div className='card'>
+                    <img src="https://via.placeholder.com/250x250" class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title"> Card camisa Polo </h5>
+                            <p class="card-text"> 79,90 </p>
+                            <button href="#" class="btn btn-primary"> COMPRAR </button>
+                        </div>
+                </div>
+                <div className='card' >
+                    <img src="https://via.placeholder.com/250x250" class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title"> Card camisa Polo </h5>
+                            <p class="card-text"> 79,90 </p>
+                            <button href="#" class="btn btn-primary"> COMPRAR </button>
+                        </div>
+                </div>
+                <div className='card' >
+                    <img src="https://via.placeholder.com/250x250" class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <h5 class="card-title"> Card camisa Polo </h5>
+                            <p class="card-text"> 79,90 </p>
+                            <button href="#" class="btn btn-primary"> COMPRAR </button>
+                        </div>
+                </div>
 
-            <p> 
-                Calça Jeans - R$ 120,00
-                <button onClick={ ()=> realizaCompra(120) }>Comprar</button>
-            </p>
+            </div>
 
-            <p>
-                Tênis Nike - R$ 110,99
-                <button onClick={ ()=> realizaCompra(110.99) }>Comprar</button>
-            </p>
+            {/* <ul className='list-group d-flex flex-row justify-content-around'>
+                <li className='list-group-item' > 
+                    <p> 
+                        Camisa Polo - R$ 79,90 
+                        <button className='btn btn-warning ' onClick={ ()=> realizaCompra(79.90) } >Comprar</button>
+                   </p>
+                </li>
+            
+
+            
+                <li className='list-group-item'>
+                    <p > 
+                        Calça Jeans - R$ 120,00
+                        <button className='btn btn-dark ' onClick={ ()=> realizaCompra(120) }>Comprar</button>
+                    </p>
+                </li>
+            
+            
+                <li className='list-group-item'>
+                    <p>
+                        Tênis Nike - R$ 110,99
+                        <button className='btn btn-info ms' onClick={ ()=> realizaCompra(110.99) }>Comprar</button>
+                    </p>
+                </li>
+            </ul> */}
+            
+
+            
         </div>
     )
 }
